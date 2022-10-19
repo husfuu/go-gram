fc319834b91c3d5fa1db07656a517abde430146b
M. Husni Nur Fadillah <muhammadhusninurfadillah@gmail.com>
2022-10-20 02:11:33 +0800

    feat(authorization)
    add generateToken and verifyToken

---
+++ auth/service.go
@@ -0,0 +1,73 @@
+package auth
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/joho/godotenv"
+)
+
+type Service interface {
+	GenerateToken(userID string) (string, error)
+	ValidateToken(encodedToken string) (*jwt.Token, error)
+}
+
+type jwtService struct {
+}
+
+func NewJWTService() *jwtService {
+	return &jwtService{}
+}
+
+const ENV_PATH = "../../.env"
+
+var _ = godotenv.Load(ENV_PATH)
+var SECRET_KEY = os.Getenv("JWT_SECRET_KEY")
+
+func (s *jwtService) GenerateToken(userID string) (string, error) {
+	claim := jwt.MapClaims{}
+	claim["user_id"] = userID
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+
+	signedToken, err := token.SignedString([]byte(os.Getenv("JWT_SECRET_KEY")))
+	if err != nil {
+		return signedToken, err
+	}
+
+	return signedToken, nil
+}
+
+func ParseToken(tokeString string) (id string, err error) {
+	token, err := jwt.Parse(tokeString, func(t *jwt.Token) (interface{}, error) {
+		if t.Method.Alg() != jwt.SigningMethodHS256.Alg() {
+			return nil, errors.New("invalid token signing method")
+		}
+
+		return []byte(os.Getenv("JWT_SECRET_KEY")), nil
+	})
+	fmt.Println("tokettt:", token)
+	if err != nil {
+		fmt.Println("error signatureee")
+		return "", err
+	}
+
+	if !token.Valid {
+		return "", errors.New("invalid token")
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+
+	if !ok {
+		return "", errors.New("invalid token")
+	}
+
+	userID := claims["user_id"].(string)
+	fmt.Println("udahhh lah: ", userID)
+	if err != nil {
+		return "", err
+	}
+
+	return userID, nil
+}
