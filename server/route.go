12a12fef9fc5e5b37429c51b967f01c657d3a590
M. Husni Nur Fadillah <muhammadhusninurfadillah@gmail.com>
2022-10-20 14:10:34 +0800

    feat(route): add comment routes

--- server/route.go
+++ server/route.go
@@ -2,13 +2,16 @@ package server
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/husfuu/go-gram/handler/commentHandler"
 	"github.com/husfuu/go-gram/handler/photoHandler"
 	"github.com/husfuu/go-gram/handler/socialmediaHandler"
 	"github.com/husfuu/go-gram/handler/userHandler"
 	"github.com/husfuu/go-gram/middleware"
+	"github.com/husfuu/go-gram/repository/commentRepository"
 	"github.com/husfuu/go-gram/repository/photoRepository"
 	"github.com/husfuu/go-gram/repository/socialmediaRepository"
 	"github.com/husfuu/go-gram/repository/userRepository"
+	commentservice "github.com/husfuu/go-gram/service/commentService"
 	"github.com/husfuu/go-gram/service/photoService"
 	"github.com/husfuu/go-gram/service/socialmediaService"
 	"github.com/husfuu/go-gram/service/userService"
@@ -16,6 +19,7 @@ import (
 )
 
 func NewRouter(r *gin.Engine, db *gorm.DB) {
+	// user routes
 	userRepo := userRepository.NewUserRepository(db)
 	userSrv := userService.NewUserService(userRepo)
 	userHdlr := userHandler.NewUserHandler(userSrv)
@@ -37,7 +41,7 @@ func NewRouter(r *gin.Engine, db *gorm.DB) {
 	photoRoute.PUT("/:photo_id", photoHdlr.Update)
 	photoRoute.DELETE("/:photo_id", photoHdlr.Delete)
 
-	// social media
+	// social media routes
 	socialMediaRepo := socialmediaRepository.NewSocialMediaRepository(db)
 	socialMediaSrv := socialmediaService.NewSocialMediaService(socialMediaRepo, photoRepo)
 	socialMediaHdlr := socialmediaHandler.NewSocialMediaHandler(socialMediaSrv)
@@ -47,4 +51,15 @@ func NewRouter(r *gin.Engine, db *gorm.DB) {
 	socialMediaRoute.GET("", socialMediaHdlr.GetSocialMedias)
 	socialMediaRoute.PUT("/:social_media_id", socialMediaHdlr.Update)
 	socialMediaRoute.DELETE("/:social_media_id", socialMediaHdlr.DeleteByID)
+
+	// comment routes
+	commentRepo := commentRepository.NewCommentRepository(db)
+	commentSrv := commentservice.NewCommentService(commentRepo)
+	commentHdlr := commentHandler.NewCommentHandler(commentSrv)
+
+	commentRoute := r.Group("/comments").Use(middleware.Authorization)
+	commentRoute.POST("", commentHdlr.Create)
+	commentRoute.GET("", commentHdlr.GetComments)
+	commentRoute.PUT("/:comment_id", commentHdlr.Update)
+	commentRoute.DELETE("/:comment_id", commentHdlr.Delete)
 }
