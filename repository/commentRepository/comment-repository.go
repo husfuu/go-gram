eab49df2bc890f6747e097937cfdafe4bece27a0
M. Husni Nur Fadillah <muhammadhusninurfadillah@gmail.com>
2022-10-20 14:08:58 +0800

    feat(repository): add comment repository

---
+++ repository/commentRepository/comment-repository.go
@@ -0,0 +1,57 @@
+package commentRepository
+
+import (
+	"fmt"
+
+	"github.com/husfuu/go-gram/entity"
+	"gorm.io/gorm"
+)
+
+type CommentRepository interface {
+	Create(comment entity.Comment) (entity.Comment, error)
+	Get() ([]entity.Comment, error)
+	Update(comment entity.Comment) (entity.Comment, error)
+	Delete(id string) error
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewCommentRepository(db *gorm.DB) CommentRepository {
+	return &repository{db}
+}
+
+func (r repository) Create(comment entity.Comment) (entity.Comment, error) {
+	err := r.db.Create(&comment).Error
+	if err != nil {
+		return entity.Comment{}, err
+	}
+	return comment, nil
+}
+
+func (r repository) Get() ([]entity.Comment, error) {
+	var comment []entity.Comment
+	err := r.db.Preload("User").Preload("Photo").Find(&comment).Error
+	if err != nil {
+		return []entity.Comment{}, err
+	}
+	return comment, nil
+}
+
+func (r repository) Update(comment entity.Comment) (entity.Comment, error) {
+	fmt.Println("comment id::::: ", comment.ID)
+	fmt.Println("new message::::::::::::", comment.Message)
+	err := r.db.Debug().Where("id = ?", comment.ID).Updates(&comment).First(&comment).Error
+	if err != nil {
+		return entity.Comment{}, err
+	}
+	return comment, nil
+}
+
+func (r repository) Delete(id string) error {
+	comment := entity.Comment{}
+	comment.ID = id
+
+	return r.db.Debug().First(&comment).Where("id = ?", comment.ID).Delete(&comment).Error
+}
